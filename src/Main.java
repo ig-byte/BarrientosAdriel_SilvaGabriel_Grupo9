9a7d1f6571040ab9aa0bfe0c4dac1b11e73b1b90
ig-byte <gabriel.silva.aravena@gmail.com>
2023-10-02 16:02:15 -0300

    comentarios

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,37 @@ public class Main {
         System.out.println("Hola mundo !!!");
         System.out.println("asda");
         System.out.println("Holaa, intento 2");
+        System.out.println();
         // TODO code application logic here
+
+        /*
+         * Generar menu con
+         * 
+         * 1. Ver asientos disponibles
+         * 2. Comprar asiento
+         * 3. Anular vuelo
+         * 4. Salir
+         */
+        /*
+         * Generar clase pasajero con parametros
+         * • nombrePasajero
+         * • rutPasajero
+         * • telefonoPasajero
+         * Reglas de negocio para pasajero
+         * - Si el pasajero es VIP hacer un descuento de un 15%
+         */
+        /*
+         * Formato de Array de asientos
+         * | 1 2 3 4 5 6 |
+         * | 7 8 9 10 11 12 |
+         * | 13 14 15 16 17 18 |
+         * | 19 20 21 22 23 24 |
+         * | 25 26 27 28 29 30 |
+         * |_____________ ____________|
+         * |_____________ ____________|
+         * | 31 32 33 34 35 36 |
+         * | 37 38 39 40 41 42 |
+         */
     }
-    
+
 }
