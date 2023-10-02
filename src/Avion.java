cfd3d0c32cf491ea8c23767a2210e458c1b10054
Adriel Barrientos <adrielgbm@gmail.com>
2023-10-02 17:54:11 -0300

    Method PoblarAvion

--- src/Avion.java
+++ src/Avion.java
@@ -1,8 +1,16 @@
-class avion {
+class Avion {
 
     Asiento[] asientosPasajeros = new Asiento[42];
 
     public void poblarAvion() {
-
+        boolean vip;
+        for (int i = 1; i <= 42; i++)
+        {
+            if (i < 31)
+                vip = false;
+            else
+                vip = true;
+            Asiento asiento = new Asiento(vip);
+        }
     }
 }
